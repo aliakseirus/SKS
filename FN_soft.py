3b486d7431e554392494dd2705ab8b4c0964f3a8
aliakseirus <xaccatleonid@gmail.com>
2021-09-03 14:27:25 +0300

    Create Tkinter window

--- FN_soft.py
+++ FN_soft.py
@@ -1,3 +1,47 @@
-print("Hello World!")
-print("Don't think")
-print("3")
+# This program can find prices on materials from user's inputs, 
+#
+# and put it in Excel file.
+#
+# Program create only for First Number LLC usage.
+
+
+# import libraries
+from tkinter import *
+from tkinter import ttk
+from tkinter import Radiobutton
+from tkinter import Checkbutton
+from tkinter import messagebox
+from tkinter import scrolledtext
+from tkinter.ttk import Combobox
+
+
+# download Datastream price
+
+
+
+
+
+# create program window
+root = Tk()
+root.title('First Number')
+
+# create menu
+main_menu = Menu()
+
+file_menu = Menu()
+file_menu.add_command(label='Datastream Price', command=None)
+file_menu.add_command(label='Avant Video Price', command=None)
+file_menu.add_command(label='Avant Skd Price', command=None)
+file_menu.add_command(label='Netair Price', command=None)
+file_menu.add_separator()
+file_menu.add_command(label='Delete all prices', command=None)
+
+main_menu.add_cascade(label='PRICES', menu=file_menu)
+main_menu.add_cascade(label='SKS', command=None)
+main_menu.add_cascade(label='EXIT', command=None)
+
+root.config(menu = main_menu)
+
+
+# launch program window
+root.mainloop()
