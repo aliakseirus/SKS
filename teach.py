506737ba0e4c22e43a84c7238b0a60d5bee2292e
aliakseirus <xaccatleonid@gmail.com>
2021-09-03 12:43:08 +0300

    Learn strings

--- teach.py
+++ teach.py
@@ -35,3 +35,52 @@ print('round(x + y) =', round(x + y))'''
 print('bin(x) =', bin(x))
 print('hex(x) =', hex(x))
 print('oct(x) =', oct(x))'''
+
+# Strings
+
+'''s1 = 'Leonid'
+s2 = 'Leon'
+
+print('s1 + s2 =', s1 + s2)
+print('s1 * 3 =', s1 * 3)
+print('len(s1) =', len(s1))
+
+print('s1[0] =', s1[0])
+print('s2[2] =', s2[2])
+print('s1[-3] =', s1[-3])
+
+s3 = 'My name is Aliaksei'
+
+print('s3[4:14] =', s3[4:14])
+print('s3[2:-2] =', s3[2:-2])
+print('s3[:6] =', s3[:6])
+print('s3[1:] =', s3[1:])
+print('s3[:] =', s3[:])
+
+print('s3[::-1] =', s3[::-1])
+print('s3[4:23:2] =', s3[4:23:2])
+print('s3[2::3] =', s3[2::3])
+
+s4 = "  I'm interesting in engineering and programming  "
+
+print('len(s4) =', len(s4))
+print("index =", s4.find('an'))
+print("index =", s4.rfind('rest'))
+print("index =", s4.index('eer'))
+print("replace() =", s4.replace('ing','GOAL'))
+print('split() = ', s4.split('in'))
+print('isdigit() =', s4.isdigit())
+print('isalpha() =', s4.isalpha())
+print('isalnum() =', s4.isalnum())
+print('islower() =', s4.islower())
+print('isupper() =', s4.isupper())
+print('isspace() =', s4.isspace())
+print('istitle() =', s4.istitle())
+print('upper() =', s4.upper())
+print('lower() =', s4.lower())
+print('startswith() =', s4.startswith('g'))
+print('endswith() =', s4.endswith('g'))
+print('count() =', s4.count('i'))
+print('lstrip() =', s4.lstrip())
+print('rstrip() =', s4.rstrip())
+print('title() =', s4.title())'''
