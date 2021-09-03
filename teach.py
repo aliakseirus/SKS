93dc1e113d8f9f3ba03da478d442b6447e1b3771
aliakseirus <xaccatleonid@gmail.com>
2021-09-03 12:06:55 +0300

    Learn integer numbers

--- teach.py
+++ teach.py
@@ -1,1 +1,20 @@
+# Here we go. Let's start study again
 
+# Numbers
+
+# Integer numbers
+
+x = 44
+y = 25
+
+print('x + y =', x + y)		
+print('x - y =', x - y)
+print('x * y =', x * y)
+print('x / y =', x / y)
+print('x // y =', x // y)
+print('x % y =', x % y)
+print('-x =', -x)
+print('abs(x) =', abs(x))
+print('divmod(x,y) =', divmod(x,y))
+print('x ** y =', x ** y)
+print('pow(x, y) =', pow(x,y))
